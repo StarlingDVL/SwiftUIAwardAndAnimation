ded661cf86f5e15d7547f399372e97e7a874e2a8
ISkvor <starlingy608@gmail.com>
2022-05-14 21:51:43 +0300

    Initial Commit

---
+++ SwiftUIAwardAndAnimation/SwiftUIAwardAndAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAwardAndAnimationApp.swift
+//  SwiftUIAwardAndAnimation
+//
+//  Created by Юрий Скворцов on 14.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAwardAndAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
