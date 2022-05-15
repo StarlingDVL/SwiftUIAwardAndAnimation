e4a7c57957a987f4cf6702b182341d3d48a63aaf
ISkvor <starlingy608@gmail.com>
2022-05-15 05:27:27 +0300

    Second

--- SwiftUIAwardAndAnimation/CenterTopView.swift
+++ SwiftUIAwardAndAnimation/CenterTopView.swift
@@ -34,6 +34,7 @@ struct CenterTopView: View {
                 path.addLine(to: CGPoint(x: middleHorizontal, y: stepY * 30))
             }
             .stroke()
+            .fill(Color.red)
             
             Path { path in
                 path.move(to: CGPoint(x: stepX * 110, y: stepY * 35))
@@ -45,6 +46,7 @@ struct CenterTopView: View {
                 path.addLine(to: CGPoint(x: stepX * 110, y: stepY * 35))
             }
             .stroke()
+            .fill(Color.green)
             
             Path { path in
                 path.move(to: CGPoint(x: stepX * 190, y: stepY * 35))
@@ -56,6 +58,7 @@ struct CenterTopView: View {
                 path.addLine(to: CGPoint(x: stepX * 190, y: stepY * 35))
             }
             .stroke()
+            .fill(Color.green)
             
             Path { path in
                 path.move(to: CGPoint(x: stepX * 130, y: stepY * 70))
@@ -63,8 +66,37 @@ struct CenterTopView: View {
                 path.addLine(to: CGPoint(x: stepX * 170, y: stepY * 70))
             }
             .stroke()
+            .fill(Color.purple)
             
+            Path { path in
+                path.move(to: CGPoint(x: stepX * 100, y: stepY * 15))
+                path.addLine(to: CGPoint(x: stepX * 85 , y: stepY * 20))
+                path.addLine(to: CGPoint(x: stepX * 71 , y: stepY * 25))
+                path.addLine(to: CGPoint(x: stepX * 58 , y: stepY * 40))
+                path.addLine(to: CGPoint(x: stepX * 54 , y: stepY * 45))
+                path.addLine(to: CGPoint(x: stepX * 47 , y: stepY * 70))
+                path.addLine(to: CGPoint(x: stepX * 45 , y: stepY * 90))
+                path.addLine(to: CGPoint(x: stepX * 45 , y: stepY * 100))
+                path.addLine(to: CGPoint(x: stepX * 90 , y: stepY * 100))
+                
+            }
+            .stroke()
+            .fill(Color.blue)
             
+            Path { path in
+                path.move(to: CGPoint(x: stepX * 200, y: stepY * 15))
+                path.addLine(to: CGPoint(x: stepX * 215 , y: stepY * 20))
+                path.addLine(to: CGPoint(x: stepX * 229 , y: stepY * 25))
+                path.addLine(to: CGPoint(x: stepX * 242 , y: stepY * 40))
+                path.addLine(to: CGPoint(x: stepX * 246 , y: stepY * 45))
+                path.addLine(to: CGPoint(x: stepX * 253 , y: stepY * 70))
+                path.addLine(to: CGPoint(x: stepX * 255 , y: stepY * 90))
+                path.addLine(to: CGPoint(x: stepX * 255 , y: stepY * 100))
+                path.addLine(to: CGPoint(x: stepX * 210 , y: stepY * 100))
+                
+            }
+            .stroke()
+            .fill(Color.blue)
         }
     }
 }
